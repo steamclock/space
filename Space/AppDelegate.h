92ce2d088ac44ec865ec2f03f13754bff9bf6447
Nigel Brooke <nigel@steamclock.com>
2013-08-15 13:51:21 -0700

    Add project template

---
+++ Space/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Space
+//
+//  Created by Nigel Brooke on 2013-08-15.
+//  Copyright (c) 2013 University of British Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
