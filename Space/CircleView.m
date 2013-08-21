e343007201c89b1cc1ece1351166e448ae5b1dda
Nigel Brooke <nigel@steamclock.com>
2013-08-21 14:50:08 -0700

    Show title on circles.

--- Space/CircleView.m
+++ Space/CircleView.m
@@ -9,16 +9,56 @@
 #import "CircleView.h"
 #import "Circle.h"
 
+@interface CircleView () {
+    Circle* _circle;
+}
+
+@property UILabel* titleLabel;
+
+@end
+
 @implementation CircleView
 
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        [self commonSetup];
     }
     return self;
 }
 
+- (id)initWithImage:(UIImage *)image {
+    self = [super initWithImage:image];
+    if (self) {
+        [self commonSetup];
+    }
+    return self;
+}
+
+-(void)commonSetup {
+    [self addObserver:self forKeyPath:@"circle" options:0 context:NULL];
+    self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.frame.size.height, self.frame.size.width, 20)];
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:self.titleLabel];
+    self.clipsToBounds = NO;
+}
+
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if([keyPath isEqualToString:@"title"]) {
+        self.titleLabel.text = self.circle.title;
+    }
+}
+
+-(void)setCircle:(Circle *)circle {
+    [_circle removeObserver:self forKeyPath:@"title"];
+    _circle = circle;
+    self.titleLabel.text = circle.title;
+    [_circle addObserver:self forKeyPath:@"title" options:0 context:NULL];
+}
+
+-(Circle*)circle {
+    return _circle;
+}
 
 -(void)setCenter:(CGPoint)center {
     [super setCenter:center];
